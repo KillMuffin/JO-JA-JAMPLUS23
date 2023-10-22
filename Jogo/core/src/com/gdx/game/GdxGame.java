d05101277821ed062cba2d64d836ae7347a65e64
KillMuffin <gabrielfranzinip1@gmail.com>
2023-10-22 18:18:41 -0300

    Knight

--- Jogo/core/src/com/gdx/game/GdxGame.java
+++ Jogo/core/src/com/gdx/game/GdxGame.java
@@ -4,52 +4,66 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.MathUtils;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img, TPC, ProjDefault;
-	private Sprite PC;
-	private float posX, posY, vel, rposX, rposY;
-	private int bulletVel, fireRate, fireAux;
+	Texture img, tPC, ProjDefault, tKnight;
+	private float posX, posY, vel, rposX, rposY, fireRate, fireAux;
+	private float enemyVelM, enemySpawnRateM;
+	private float knightVel;
+	private int bulletVel;
 	private List<float[]> PProjectiles = new ArrayList<float[]>();
+	private List<float[]> Knights = new ArrayList<float[]>();
+	private long lastSpawnTime;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("img/PHBG.png");
-		TPC = new Texture("img/entities/PC/PHKingPoring.png");
+		tPC = new Texture("img/entities/PC/PHKingPoring.png");
 		ProjDefault = new Texture("img/entities/projectiles/PHdefault.png");
-		PC = new Sprite(TPC);
+		tKnight = new Texture("img/entities/knight/PHgoblin.png");
 		posX = 0; 
 		posY = 0; 
-		rposX = Gdx.graphics.getWidth() / 2 - PC.getWidth() / 2; 
-		rposY = Gdx.graphics.getHeight() / 2 - PC.getHeight() / 2; 
-		vel = 2;
+		rposX = Gdx.graphics.getWidth() / 2; 
+		rposY = Gdx.graphics.getHeight() / 2; 
+		vel = 4;
+		enemyVelM = 1;
+		knightVel = 1;
 		bulletVel = 3;
 		fireRate = 1;
 		fireAux = 60;
+		lastSpawnTime = 0;
 	}
 
 	@Override
 	public void render () {
 
 		this.movePC();
-		this.PProjectilesHandling();
+		this.handlePProjectiles();
+		this.spawnKnight();
+		this.handleKnights();
 
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, posX, posY);
-		batch.draw(PC, rposX, rposY);
+		batch.draw(tPC, rposX - tPC.getWidth() / 2, rposY - tPC.getHeight() / 2);
 		
 		for (int i=0; i < PProjectiles.size(); i++) {
-			float[] i2 = PProjectiles.get(i);
-			batch.draw(ProjDefault, i2[3], i2[4]);
+			float[] projectile = PProjectiles.get(i);
+			batch.draw(ProjDefault, projectile[3], projectile[4]);
 		} 
+
+		for (int i=0; i < Knights.size(); i++) {
+			float[] knight = Knights.get(i);
+			batch.draw(tKnight, knight[1], knight[2]);
+		}
 
 		batch.end();
 	}
@@ -58,11 +72,12 @@ public class GdxGame extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
-		TPC.dispose();
+		tPC.dispose();
 		ProjDefault.dispose();
 	}
 
 	private void movePC() {
+
 		if (Gdx.input.isKeyPressed(Input.Keys.D) && posX > -Gdx.graphics.getWidth()) {
 			posX -= vel;
 		}
@@ -77,7 +92,7 @@ public class GdxGame extends ApplicationAdapter {
 		}
 	}
 
-	private void PProjectilesHandling() {
+	private void handlePProjectiles() {
 		if (fireAux > 0) {
 			fireAux -= fireRate;
 		}
@@ -125,6 +140,51 @@ public class GdxGame extends ApplicationAdapter {
 			}
 			else {
 				PProjectiles.remove(i);
+			}
+		}
+	}
+
+	private void spawnKnight() {
+		float[] knight = new float[7];
+		
+
+		knight[0] = 2;
+		knight[1] = MathUtils.random(0, 1280);
+		knight[2] = MathUtils.random(0, 720);
+		knight[3] = posX;
+		knight[4] = posY;
+
+		Knights.add(knight);
+		
+		lastSpawnTime = TimeUtils.nanoTime();
+	}
+
+	private void handleKnights() {
+		
+		// if (TimeUtils.nanoTime() - lastSpawnTime > 1000000000) {
+		// 	this.spawnEnemies();
+		// }
+
+		for (int i=0; i < Knights.size(); i++) {
+			float[] knight = Knights.get(i);
+
+			float velX = rposX - knight[1];
+			float velY = rposY - knight[2];
+
+			float length = (float) Math.sqrt(velX * velX + velY * velY);
+			velX *= (knightVel * enemyVelM) / length;
+			velY *= (knightVel * enemyVelM) / length;
+			
+			knight[1] += velX; 
+			knight[2] += velY;
+
+			knight[1] -= knight[3] - posX;
+			knight[2] -= knight[4] - posY;
+			knight[3] = posX;
+			knight[4] = posY;
+
+			if (knight[0] <= 0) {
+				Knights.remove(knight);
 			}
 		}
 	}
